9676c4d921b31bbf2cd1f1489c917d9bf3a62a2a
yinlong22 <342241857@.com>
2020-05-15 21:43:53 +0800

    使用 chai.expect 添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -6,18 +6,78 @@ import ButtonGroup from './button-group'
 //注册
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
-Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
-    data:{
-        loading1:false,
-        loading2:true,
-        loading3:false
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false
     }
 })
 
 //单元测试
+import chai from 'chai'
+
+const expect = chai.expect
 {
-    console.log(Button)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-set')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'set',
+        }
+    })
+    vm.$mount()
+    vm.$on('click', function () {
+        expect(1).to.eq(1)
+    })
+    let button = vm.$el
+    button.click()
+    button.$el.remove()
+    button.$destroy()
 }
