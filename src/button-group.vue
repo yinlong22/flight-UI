7a132376cba41015d3cfc8660c013aa1c51cd4c0
yinlong22 <342241857@.com>
2020-05-15 17:01:23 +0800

    第一个元素不需要往左移 1像素

--- src/button-group.vue
+++ src/button-group.vue
@@ -24,7 +24,10 @@
 
         > .g-button {
             border-radius: 0;
-            margin-left: -1px;
+
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
 
             &:first-child {
                 border-top-left-radius: var(--border-radius);
