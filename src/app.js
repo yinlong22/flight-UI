aa27790d228b34ceefdea63ae49962981e945bdf
yinlong22 <342241857@.com>
2020-05-15 16:55:51 +0800

    检测子元素是不是 button

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group'
 
 //注册
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group',ButtonGroup)
 
 new Vue({
     el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="g-button-group">
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            for (let node of this.$el.children) {
+                let name = node.nodeName.toLowerCase()
+                if (name !== 'button') {
+                    console.warn(`g-button-group的子元素应该全是button,但是你写的是 ${name}`)
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        > .g-button {
+            border-radius: 0;
+            margin-left: -1px;
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:last-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
