a0a814b596449058259de7c0a97c91799e7205e4
yinlong22 <342241857@.com>
2020-05-16 11:29:27 +0800

    准备 karma 测试

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
-    @click="$emit('click')">
+            @click="$emit('click')">
         <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
         <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
         <div class="content">
@@ -10,8 +10,15 @@
 </template>
 
 <script>
+    // import Vue from 'vue'
+    // Vue.component('g-icon',Icon)
+    import Icon from './Icon'
+
     export default {
         // props: ['icon', 'iconPosition']
+        components: {
+            'g-icon': Icon
+        },
         props: {
             icon: {},
             loading: {
