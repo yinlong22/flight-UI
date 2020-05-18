f955574d88c2dee8b6b5270046b3ebd21946e172
yinlong22 <342241857@.com>
2020-05-18 18:36:48 +0800

    添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="sider">
+    <transition name="slide">
+    <div class="sider" v-if="visible">
         <slot></slot>
+        <button @click="visible=false">close</button>
     </div>
+    </transition>
 </template>
 <script>
     export default {
-        name:'fliSider'
+        name: 'fliSider',
+        data() {
+            return {
+                visible: true
+            }
+        },
+        methods:{}
     }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
     .sider {
+        position: relative;
 
+        > button {
+            position: absolute;
+            right: 0;
+            top: 0;
+        }
+    }
+    .slide-enter-active, .slide-leave-active{
+        transition: all .3s;
+    }
+    .slide-enter, .slide-leave-to{
+         margin-left: -200px;
     }
 </style>
