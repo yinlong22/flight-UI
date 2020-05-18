048889bec1207b005473fb8dbcba6449f9d1ba55
yinlong22 <342241857@.com>
2020-05-18 13:57:30 +0800

    重构并完成响应式

--- src/col.vue
+++ src/col.vue
@@ -1,14 +1,83 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="colClass" :style="colStyle">
         <slot></slot>
     </div>
 </template>
 <script>
+    let validator = (value) => {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach(key => {
+            if (![`span`, `offset`].includes(key)) {
+                valid = false
+            }
+        })
+        return valid
+    }
+
     export default {
         name: 'fliCol',
         props: {
             span: {
                 type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            },
+            iPad: {
+                type: Object,
+                validator,
+            },
+            narrowPc: {
+                type: Object,
+                validator,
+            },
+            pc: {
+                type: Object,
+                validator,
+            },
+            widePc: {
+                type: Object,
+                validator,
+            }
+        },
+        data() {
+            return {
+                gutter: 0,
+            }
+        },
+        methods: {
+            createClasses(obj, str = '') {
+                if (!obj) {
+                    return []
+                }
+                let array = []
+                if (obj.span) {
+                    array.push(`col-${str}${obj.span}`)
+                }
+                if (obj.offset) {
+                    array.push(`offset-${str}${obj.offset}`)
+                }
+                return array
+            }
+        },
+        computed: {
+            colClass() {
+                let {span, offset, iPad, narrowPc, pc, widePc} = this
+                let createClasses = this.createClasses
+                return [
+                    ...createClasses({span, offset}),
+                    ...createClasses(iPad, 'iPad-'),
+                    ...createClasses(narrowPc, 'narrowPc-'),
+                    ...createClasses(pc, 'pc-'),
+                    ...createClasses(widePc, 'widePc-')
+                ]
+            },
+            colStyle() {
+                return {
+                    paddingRight: this.gutter / 2 + 'px',
+                    paddingLeft: this.gutter / 2 + 'px',
+                }
             }
         }
     }
@@ -16,9 +85,9 @@
 <style lang="scss" scoped>
     .col {
         height: 100px;
-        background: salmon;
+        background: mediumaquamarine;
         width: 50%;
-        border: 1px solid blue;
+        border: 1px solid wheat;
 
         $class-prefix: col-;
         @for $n from 1 through 24 {
@@ -26,5 +95,68 @@
                 width: ($n / 24) * 100%;
             }
         }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+        @media (min-width: 577px) {
+            $class-prefix: col-ipad-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / 24) * 100%;
+                }
+            }
+            $class-prefix: offset-iPad-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n / 24) * 100%;
+                }
+            }
+        }
+        @media (min-width: 769px) {
+            $class-prefix: col-narrowPc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / 24) * 100%;
+                }
+            }
+            $class-prefix: offset-narrowPc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n / 24) * 100%;
+                }
+            }
+        }
+        @media (min-width: 993px) {
+            $class-prefix: col-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / 24) * 100%;
+                }
+            }
+            $class-prefix: offset-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n / 24) * 100%;
+                }
+            }
+        }
+        @media (min-width: 1201px) {
+            $class-prefix: col-widePc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    width: ($n / 24) * 100%;
+                }
+            }
+            $class-prefix: offset-widePc-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n} {
+                    margin-left: ($n / 24) * 100%;
+                }
+            }
+        }
     }
+
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,11 +1,55 @@
 <template>
-    <div class="row">
+    <div class="row" :style="rowStyle" :class="rowClass">
         <slot></slot>
     </div>
 </template>
-
+<script>
+    export default {
+        name: 'fliRow',
+        props: {
+            gutter: {
+                type: [Number, String]
+            },
+            align: {
+                type: String,
+                validator(value) {
+                    return ['left', 'right', 'center'].includes(value)
+                }
+            }
+        },
+        computed: {
+            rowStyle() {
+                let {gutter} = this
+                return {
+                    marginLeft: -gutter / 2 + 'px'
+                    , marginRight: -gutter / 2 + 'px'
+                }
+            },
+            rowClass() {
+                let {align} = this
+                return [align && `align-${align}`]
+            }
+        },
+        mounted() {
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
+    }
+</script>
 <style lang="scss" scoped>
-    .row{
+    .row {
         display: flex;
+        &align-left {
+            justify-content: flex-start;
+        }
+
+        &align-left {
+            justify-content: flex-end;
+        }
+
+        &align-center {
+            justify-content: center;
+        }
     }
 </style>
