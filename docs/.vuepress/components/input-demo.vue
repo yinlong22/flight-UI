4705fb7072708e3f66943ab10d41b55fe873b0a3
yinlong22 <342241857@.com>
2020-05-21 23:17:31 +0800

    完成 input 组件部署

---
+++ docs/.vuepress/components/input-demo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <f-input value="张三"></f-input>
+        <f-input value="李四" :disabled="true"></f-input>
+        <f-input value="王五" readonly></f-input>
+        <f-input value="王" error="姓名不能少于两个字"></f-input>
+        <p>
+        <f-input value="王" v-model="message"></f-input> : {{message}}
+        </p>
+    <pre><code>{{content}}</code></pre>
+    </div>
+</template>
+<script>
+    import Input from '../../../src/input'
+
+    export default {
+        components: {
+            'f-input': Input,
+        },
+        data() {
+            return {
+                message: 'hello',
+                content: `
+        message: 'hello',
+        <f-input value="张三"></f-input>
+        <f-input value="李四" :disabled="true"></f-input>
+        <f-input value="王五" readonly></f-input>
+        <f-input value="王" error="姓名不能少于两个字"></f-input>
+        <p>
+        <f-input value="王" v-model="message"></f-input>{{message}}
+        </p>
+      `
+            }
+        },
+    }
+</script>
