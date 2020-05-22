1de2c63a5e792af382301ba85a561ea447f2f6c4
yinlong22 <342241857@.com>
2020-05-22 18:48:15 +0800

    更新介绍

--- docs/.vuepress/components/collapse-demo.vue
+++ docs/.vuepress/components/collapse-demo.vue
@@ -3,15 +3,15 @@
         <f-collapse :selected.sync="selectedTab">
             <f-collapse-item title="水果" name="1">
                 <img height="300px"
-                     src="https://user-gold-cdn.xitu.io/2020/5/22/1723b8bf39b3586d?w=500&h=445&f=png&s=105031">
+                     src="https://camo.githubusercontent.com/34166ee79785af08bdff3d4a07cc8de31ba40cf8/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f32322f313732336238626633396233353836643f773d35303026683d34343526663d706e6726733d313035303331">
             </f-collapse-item>
             <f-collapse-item title="蛋糕" name="2">
                 <img height="300px"
-                     src="https://user-gold-cdn.xitu.io/2020/5/22/1723b8ca6ac03ced?w=1000&h=667&f=png&s=1062604">
+                     src="https://camo.githubusercontent.com/454499020672f4adb9ec636baec98f775e34256e/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f32322f313732336238636136616330336365643f696d61676556696577322f302f772f313238302f682f3936302f666f726d61742f776562702f69676e6f72652d6572726f722f31">
             </f-collapse-item>
             <f-collapse-item title="哈士奇" name="3">
                 <img height="300px"
-                     src="https://user-gold-cdn.xitu.io/2020/5/22/1723b8f5e2072cd8?w=440&h=440&f=png&s=239450">
+                     src="https://camo.githubusercontent.com/8860754c7d7e53af94b79268a25870e57191e921/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f32322f313732336238663565323037326364383f773d34343026683d34343026663d706e6726733d323339343530">
             </f-collapse-item>
         </f-collapse>
 
@@ -35,18 +35,18 @@
             data() {selectedTab: ['2']
             // selectedTab: ['1','2']
             }
-            <f-collapse :selected.sync="selectedTab">
+       <f-collapse :selected.sync="selectedTab">
             <f-collapse-item title="水果" name="1">
                 <img height="300px"
-                     src="https://user-gold-cdn.xitu.io/2020/5/22/1723b8bf39b3586d?w=500&h=445&f=png&s=105031">
+                     src="https://camo.githubusercontent.com/34166ee79785af08bdff3d4a07cc8de31ba40cf8/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f32322f313732336238626633396233353836643f773d35303026683d34343526663d706e6726733d313035303331">
             </f-collapse-item>
             <f-collapse-item title="蛋糕" name="2">
                 <img height="300px"
-                     src="https://user-gold-cdn.xitu.io/2020/5/22/1723b8ca6ac03ced?w=1000&h=667&f=png&s=1062604">
+                     src="https://camo.githubusercontent.com/454499020672f4adb9ec636baec98f775e34256e/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f32322f313732336238636136616330336365643f696d61676556696577322f302f772f313238302f682f3936302f666f726d61742f776562702f69676e6f72652d6572726f722f31">
             </f-collapse-item>
             <f-collapse-item title="哈士奇" name="3">
                 <img height="300px"
-                     src="https://user-gold-cdn.xitu.io/2020/5/22/1723b8f5e2072cd8?w=440&h=440&f=png&s=239450">
+                     src="https://camo.githubusercontent.com/8860754c7d7e53af94b79268a25870e57191e921/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f32322f313732336238663565323037326364383f773d34343026683d34343026663d706e6726733d323339343530">
             </f-collapse-item>
         </f-collapse>
       `.replace(/\s{2,}/, '').trim()
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,7 @@ fil-ui我在学习 Vue 的过程中做的一个好用的简洁型 UI 库，适�
 
 各组件已完成BDD测试模式的单元测试，并部署到Travis Ci
 <div style="padding-top: -200px">
-<img height="500px" src="https://user-gold-cdn.xitu.io/2020/5/22/1723bbf20a61da0d?w=800&h=800&f=png&s=407331">
+<img height="500px" src="https://camo.githubusercontent.com/2356d32350b11d4cd3eb5eb7bf7a8d2033a2f152/68747470733a2f2f757365722d676f6c642d63646e2e786974752e696f2f323032302f352f32322f313732336262663230613631646130643f773d38303026683d38303026663d706e6726733d343037333331">
 </div>
 
 
